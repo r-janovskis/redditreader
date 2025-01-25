650aabebd485d405a83b0d5d29871ebe5d4a713b
Reins Janovskis <reinis.janovskis@hotmail.com>
2025-01-25 14:22:28 +0000

    Add logo.

---
+++ app/components/logo/Logo.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./Logo.module.css";
+import image from "./../../logo.png";
+
+export const Logo = () => {
+    return (
+        <img
+            src='./logo.png'
+            alt="Reddit Reader Logo"
+        />
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@ import type { Metadata } from "next";
 import { Counter } from "./components/counter/Counter";
 
 export default function IndexPage() {
-  return <Counter />;
+  return (
+    <>
+      <Counter />
+    </>
+  );
 }
 
 export const metadata: Metadata = {
-  title: "Redux Toolkit",
+  title: "Reddit Reader",
 };
