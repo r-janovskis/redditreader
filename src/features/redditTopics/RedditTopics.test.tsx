cbab404cf082c233a66cbd95518b04e97bdc8349
Reins Janovskis <reinis.janovskis@hotmail.com>
2025-02-01 21:46:24 +0000

    Play with test to get handle on Jest, close #5

---
+++ src/features/redditTopics/RedditTopics.test.tsx
@@ -0,0 +1,28 @@
+import { screen, waitFor } from "@testing-library/react";
+import { renderWithProviders } from "./../../utils/test-utils";
+import { RedditTopics } from './RedditTopics';
+
+describe('RedditTopics', () => {
+
+    it('renders correctly', () => {
+        expect(RedditTopics).toBeDefined();
+    })
+
+    test("Can we get field value using label text", () => {
+        renderWithProviders(<RedditTopics />)
+    
+        expect(screen.getByLabelText("Reddit Topic:")).toBeInTheDocument()
+        expect(screen.getByLabelText("Reddit Topic:")).toHaveTextContent(/nature/i)
+    })
+
+    it("Reddit Topic should changes to selected option - books", () => {
+        const { user } = renderWithProviders(<RedditTopics />)
+
+        user.selectOptions(screen.getByLabelText("Reddit Topic:"), "Books")
+        expect(screen.getByLabelText("Reddit Topic:")).toHaveTextContent(/books/i)
+
+    })
+
+});
+
+
--- src/features/redditTopics/RedditTopics.tsx
+++ src/features/redditTopics/RedditTopics.tsx
@@ -4,13 +4,16 @@ import styles from "./RedditTopics.module.css";
 
 export const RedditTopics = () => {
     return (
-        <select className={styles.redditTopics}>
-            <option value="r/nature">Nature</option>
-            <option value="r/hiking">Hiking</option>
-            <option value="r/photography">Photography</option>
-            <option value="r/books">Books</option>
-            <option value="r/basketball">basketball</option>
-            <option value="r/crossfit">Crossfit</option>
-        </select>
+        <>
+            <label htmlFor = "redditTopics"> Reddit Topic: </label>
+            <select id="redditTopics" className={styles.redditTopics}>
+                <option value="r/nature">Nature</option>
+                <option value="r/hiking">Hiking</option>
+                <option value="r/photography">Photography</option>
+                <option value="r/books">Books</option>
+                <option value="r/basketball">basketball</option>
+                <option value="r/crossfit">Crossfit</option>
+            </select>
+        </>
     );
 };
