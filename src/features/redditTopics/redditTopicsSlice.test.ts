5cbe3e7ec9c3a7690a3ad3c26e014cf0e3b85a0a
Reins Janovskis <reinis.janovskis@hotmail.com>
2025-02-18 21:24:05 +0000

    Update tests to represent new states of slices in app

--- src/features/redditTopics/redditTopicsSlice.test.ts
+++ src/features/redditTopics/redditTopicsSlice.test.ts
@@ -8,7 +8,7 @@ interface LocalTestContext {
   store: AppStore;
 }
 
-const initialPosts = await fetchRedditPosts({end_point: "r/nature", title: "Nature", numberOfPosts:10});
+const initialPosts = await fetchRedditPosts({end_point: "r/nature", title: "Nature"});
 
 describe<LocalTestContext>("redditTopics reducer", it => {
   beforeEach<LocalTestContext>(context => {
@@ -16,8 +16,6 @@ describe<LocalTestContext>("redditTopics reducer", it => {
       title: "Nature",
       link_name: "nature",
       end_point: "r/nature",
-      numberOfPosts: 10,
-      step: 10,
       posts: initialPosts.data,
       status: "idle"
     }
@@ -41,7 +39,7 @@ describe<LocalTestContext>("redditTopics reducer", it => {
 it("should handle changeTopic", async ({ store }) => {
   expect(store.getState().redditTopics.title).toBe("Nature")
 
-  await store.dispatch(changeTopic({title: "Science", numberOfPosts: 10, end_point: "r/science"}))
+  await store.dispatch(changeTopic({title: "Science",  end_point: "r/science"}))
   expect(store.getState().redditTopics.title).toBe("Science")
 })
 
@@ -50,10 +48,10 @@ it("should handle changeTopic", async ({ store }) => {
 it("should handle changeTopic Back to nature (reset topic)", async ({ store}) => {
   expect(store.getState().redditTopics.title).toBe("Nature")
 
-  await store.dispatch(changeTopic({title: "Books", numberOfPosts: 10, end_point: "r/books"}))
+  await store.dispatch(changeTopic({title: "Books", end_point: "r/books"}))
   expect(store.getState().redditTopics.title).toBe("Books")
 
-  await store.dispatch(changeTopic({title: "Nature", numberOfPosts: 10, end_point: "r/nature"}))
+  await store.dispatch(changeTopic({title: "Nature", end_point: "r/nature"}))
   expect(store.getState().redditTopics.title).toBe("Nature")
   expect(store.getState().redditTopics.link_name).toBe("nature")
   expect(store.getState().redditTopics.end_point).toBe("r/nature")
